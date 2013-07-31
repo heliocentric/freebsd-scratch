8c858e99a7d543184a84487f8258ec51b8d6c5a3
Dylan Cochran <heliocentric@gmail.com>
2013-07-31 20:03:58 +0000

    Add more examples.

--- lib/libV/dial.c
+++ lib/libV/dial.c
@@ -3,11 +3,14 @@
 
 /*
 	Routine that 'dials' a remote host, and returns a list of file descriptors.
+	Similar in function to the plan 9 dial() function.
+	address and local take the following form:
+
 	Protocols:
-		tcp - tcp protocol
-		udp - udp protocol
-		sctp - sctp protocol
-		unix - unix domain socket
+		tcp - tcp protocol - form: tcp!host!port
+		udp - udp protocol - form: udp!host!port
+		sctp - sctp protocol - form: sctp!host!port
+		unix - unix domain socket - form: unix!/absolute/path or unix!relative/path
 */
 handle * dial(char * address, char * local) {
 

6602cd418a536f20bced47e2bdc3cff2bc8cda2a
Dylan Cochran <heliocentric@gmail.com>
2013-07-31 19:59:08 +0000

    Add dial function skeleton.

--- lib/libV/dial.c
+++ lib/libV/dial.c
@@ -1,1 +1,14 @@
 #include "V.h"
+
+
+/*
+	Routine that 'dials' a remote host, and returns a list of file descriptors.
+	Protocols:
+		tcp - tcp protocol
+		udp - udp protocol
+		sctp - sctp protocol
+		unix - unix domain socket
+*/
+handle * dial(char * address, char * local) {
+
+}
